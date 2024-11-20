a7525a8f53843b9837897716f4b6b97d9881e7a7
Clivon Osire <clivon254@icloud.com>
2024-11-20 11:54:06 +0300

    grid

--- admin/src/components/Stats.jsx
+++ admin/src/components/Stats.jsx
@@ -117,7 +117,7 @@ export default function Stats() {
 
         {statsLoading && !statsError && (
 
-            <div className="grid gid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-y-5 gap-x-3 ">
+            <div className="grid gid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-y-5 gap-x-3 ">
 
                     {loader.map((stat,index) => (
 
